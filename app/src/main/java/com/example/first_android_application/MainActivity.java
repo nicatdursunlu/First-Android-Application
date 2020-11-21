c60968cff5ac95972ad5eef327c215b3fc16a638
Nicat DUrsunlu <nicat.dursunlu@gmail.com>
2020-11-21 19:44:59 +0400

    my first mobile app

--- app/src/main/java/com/example/first_android_application/MainActivity.java
+++ app/src/main/java/com/example/first_android_application/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.first_android_application;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClick(View view) {
+
+        // show message to user
+        Toast.makeText(this, "You press this button", Toast.LENGTH_LONG).show();
+
+        // change text of TextView
+        TextView tv = findViewById(R.id.hello_text);
+        tv.setText("You clicked it!");
     }
 }
